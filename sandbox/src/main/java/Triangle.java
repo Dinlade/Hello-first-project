3694b1d90859a27b85f8eef45424d872bc2331c0
Dinar Saliakhov <sdm.kzn99@gmail.com>
2025-10-27 17:13:40 +0300

    стороны треугольника и периметр

---
+++ sandbox/src/main/java/Triangle.java
@@ -0,0 +1,8 @@
+public class Triangle {
+    public static void main(String[] args) {
+        var side1 = 5.;
+        var side2 = 10.;
+        var side3 = 40.;
+        System.out.println("Периметр реугольнака = " + (side1 + side2 + side3));
+    }
+}
