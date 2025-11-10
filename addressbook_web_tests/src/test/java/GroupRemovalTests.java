5ac6f12adcea6644add4a68bbc3a67bd29ec6a59
Dinar <sdm199914@yandex.ru>
2025-11-10 23:27:38 +0300

    коммит

--- addressbook_web_tests/src/test/java/GroupRemovalTests.java
+++ addressbook_web_tests/src/test/java/GroupRemovalTests.java
@@ -1,3 +1,4 @@
+import model.GroupData;
 import org.junit.jupiter.api.Test;
 
 public class GroupRemovalTests extends TestBase {
@@ -6,7 +7,7 @@ public class GroupRemovalTests extends TestBase {
     public void canRemoveGroup() {
         openGroupsPage();
         if (!isGroupPresent()){
-            createGroup("asd", "asd", "asdd");
+            createGroup(new GroupData("name", "header", "footer"));
         }
         removeGroup();
     }
