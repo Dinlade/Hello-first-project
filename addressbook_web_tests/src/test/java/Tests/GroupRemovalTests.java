b87142fccb3d2137328e58940d58a66adce1889b
Dinar <sdm199914@yandex.ru>
2025-11-12 13:26:35 +0300

    раскладка по пакетам

---
+++ addressbook_web_tests/src/test/java/Tests/GroupRemovalTests.java
@@ -0,0 +1,17 @@
+package Tests;
+
+import model.GroupData;
+import org.junit.jupiter.api.Test;
+
+public class GroupRemovalTests extends TestBase {
+
+    @Test
+    public void canRemoveGroup() {
+        app.openGroupsPage();
+        if (!app.isGroupPresent()){
+            app.createGroup(new GroupData("name", "header", "footer"));
+        }
+        app.removeGroup();
+    }
+
+}
