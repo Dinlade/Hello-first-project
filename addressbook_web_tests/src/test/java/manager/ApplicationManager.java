d929d263e938c2522bfbd5bbed794517b1f551ab
Dinar <sdm199914@yandex.ru>
2025-11-16 14:47:26 +0300

    Задание 7 и 8

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -4,16 +4,24 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class ApplicationManager {
-    protected  WebDriver driver;
+    protected WebDriver driver;
     private LoginHelper session;
     private GroupHelper groups;
+    private ContactHelper contact;
 
-    public void init() {
+    public void init(String browser) {
         if (driver == null) {
-            driver = new FirefoxDriver();
+            if ("firefox".equals(browser)) {
+                driver = new FirefoxDriver();
+            } else if ("chrome".equals(browser)) {
+                    driver = new ChromeDriver();
+            } else {
+                throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
+            }
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
             driver.get("http://localhost/addressbook/");
             driver.manage().window().setSize(new Dimension(1247, 977));
@@ -28,11 +36,18 @@ public class ApplicationManager {
         return session;
     }
 
-    public  GroupHelper groups() {
+    public GroupHelper groups() {
         if (groups == null) {
             groups = new GroupHelper(this);
         }
         return groups;
+    }
+
+    public ContactHelper contact() {
+        if (contact == null) {
+            contact = new ContactHelper(this);
+        }
+        return contact;
     }
 
     protected boolean isElementPresent(By aNew) {
@@ -43,5 +58,4 @@ public class ApplicationManager {
             return false;
         }
     }
-
 }
