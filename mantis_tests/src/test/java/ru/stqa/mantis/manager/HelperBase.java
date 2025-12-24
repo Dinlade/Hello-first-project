67383a665541019232d41a5691e9452a746210af
Dinar <sdm199914@yandex.ru>
2025-12-24 22:48:32 +0300

    Задание 17

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/HelperBase.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/HelperBase.java
@@ -1,29 +1,30 @@
-package manager;
+package ru.stqa.mantis.manager;
 
 import org.openqa.selenium.By;
 
 import java.nio.file.Paths;
 
 public class HelperBase {
-    protected final ApplicationManager manager;
 
+    protected final ApplicationManager manager;
     public HelperBase(ApplicationManager manager) {
         this.manager = manager;
     }
 
     protected void click(By locator) {
-        manager.driver.findElement(locator).click();
+        manager.driver().findElement(locator).click();
     }
 
     protected void type(By locator, String text) {
         click(locator);
-        manager.driver.findElement(locator).clear();
-        manager.driver.findElement(locator).sendKeys(text);
+        manager.driver().findElement(locator).clear();
+        manager.driver().findElement(locator).sendKeys(text);
     }
+
     protected void attach(By locator, String file) {
-        manager.driver.findElement(locator).sendKeys(Paths.get(file).toAbsolutePath().toString());
+        manager.driver().findElement(locator).sendKeys(Paths.get(file).toAbsolutePath().toString());
     }
     protected boolean isElementPresent(By locator) {
-       return manager.driver.findElements(locator).size() > 0;
+        return manager.driver().findElements(locator).size() > 0;
     }
 }
